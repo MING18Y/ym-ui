20c52f4f9057a4366e607b09c7247ab298e9804e
MING18Y <519338858@qq.com>
2020-04-03 16:25:53 +0800

    feat: webpack config ++

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import DraggableButton from '../component/draggable_button/DraggableButton.jsx'
+import Ripple from '../component/ripple/ripple.jsx'
+
+export default YMUI = { 
+    DraggableButton,
+    Ripple
+} 
+
+export { 
+    DraggableButton,
+    Ripple
+} 
---
+++ webpack.base.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: path.resolve(__dirname, 'dist'),
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader",
+                    sourceMap: true,
+                }, {
+                    loader: "less-loader",
+                    sourceMap: true
+                }]
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    }
+}
